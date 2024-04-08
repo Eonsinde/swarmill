0647ffa144b9f0821b0e7564cb0c66f2a84fc82b
eonsinde <olasinde.eon@gmail.com>
2024-04-08 21:27:58 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,134 @@
-import Image from 'next/image'
+import Link from "next/link"
+import Image from "next/image"
+import { ArrowRight } from "lucide-react"
+import MaxWidthWrapper from "@/components/max-width-wrapper"
+import { buttonVariants } from "@/components/ui/button"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+const Home = () => {
+    return (
+        <>
+            <MaxWidthWrapper className="flex flex-col justify-center items-center mb-12 mt-28 sm:mt-40 text-center">
+                <div className="flex justify-center items-center space-x-2 max-w-fit mx-auto mb-4 py-2 px-7 bg-white hover:bg-white-50 border border-gray-200 hover:border-gray-300 shadow-md backdrop-blur rounded-full overflow-hidden transition-all">
+                    <p className="text-sm font-semibold text-gray-700">
+                        Swarmill is now active!
+                    </p>
+                </div>
+                <h1 className="max-w-4xl font-bold text-5xl md:text-6xl lg:text-7xl">
+                    Have a chat with your <span className="text-rose-600">Documents</span> in seconds
+                </h1>
+                <p className="mt-5 max-w-prose sm:text-lg text-muted-foreground">
+                    Smarmill allows you to have conversations with any PDF document. Simply upload your file and start asking question right away
+                </p>
+                <Link
+                    className={buttonVariants({
+                        size: "lg",
+                        className: "mt-5"
+                    })}
+                    href="/dashboard"
+                    target="_blank"
+                >
+                    Get Started <ArrowRight className="ml-2 h-5 w-5" />
+                </Link>
+            </MaxWidthWrapper>
+            {/* value proposition section */}
+            <div>
+                <div className="relative isolate">
+                    <div aria-hidden="true" className="pointer-events-none absolute -top-40 sm:-top-80 -z-10 blur-3xl transform-gpu overflow-hidden">
+                        <div
+                            style={{
+                                clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                            }}
+                            className="relative left-[calc(50%-11rem)] sm:left-[calc(50%-20rem)] aspect-[1155/678] w-[36.125rem] sm:w-[72.1875rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b1] to-[#fcaa89] opactiy-20"
+                        />
+                    </div>
+                    <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                        <div className="mt-16 sm:mt-24 flow-root">
+                            <div className="bg-gray-900/5 -m-2 lg:-m-4 p-2 lg:p-4 ring-1 ring-inset ring-gray-900/10 rounded-xl">
+                                <Image
+                                    className="bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 rounded-md"
+                                    src="/images/dashboard-preview.jpg"
+                                    alt="product preview"
+                                    width={1364}
+                                    height={866}
+                                    quality={100}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div aria-hidden="true" className="pointer-events-none absolute right-0 -bottom-40 sm:-bottom-80 -z-10 blur-3xl transform-gpu overflow-hidden">
+                        <div
+                            style={{
+                                clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                            }}
+                            className="relative right-[calc(50%-13rem)] sm:right-[calc(50%-26rem)] aspect-[1155/678] w-[36.125rem] sm:w-[72.1875rem] translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b1] to-[#fcaa89] opactiy-20"
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* feature section */}
+            <div className="mx-auto max-w-5xl mb-32 mt-32 sm:mt-56">
+                <div className="mx-auto max-w-2xl sm:text-center">
+                    <h2 className="mt-2 font-bold text-4xl sm:text-5xl">
+                        Start chatting in minutes
+                    </h2>
+                    <p className="mt-4 text-lg text-muted-foreground">
+                        Chatting to your PDF files has been easier than with Smarmill
+                    </p>
+                </div>
+                {/* steps */}
+                <ol className="my-8 pt-8 md:flex md:space-x-12 space-y-4 md:space-y-0">
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 py-2 md:pb-0 md:pt-4 pl-4 md:pl-0 border-border border-t-2 border-l-4 md:border-l-0">
+                            <span className="text-sm font-medium text-rose-600">Step 1</span>
+                            <span className="text-xl font-semibold">Get Started</span>
+                            <span className="mt-2 text-secondary-foreground">
+                                Sign up for an account to begin. Upgrade to{" "}
+                                <Link
+                                    className="text-rose-700 underline underline-offset-2"
+                                    href="/pricing"
+                                >
+                                    pro plan
+                                </Link>
+                                {" "}for more access and features.
+                            </span>
+                        </div>
+                    </li>
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 py-2 md:pb-0 md:pt-4 pl-4 md:pl-0 border-border border-t-2 border-l-4 md:border-l-0">
+                            <span className="text-sm font-medium text-rose-600">Step 2</span>
+                            <span className="text-xl font-semibold">Upload your PDF file</span>
+                            <span className="mt-2 text-secondary-foreground">
+                                We&apos;ll process your file an make it ready for you to converse with.
+                            </span>
+                        </div>
+                    </li>
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 py-2 md:pb-0 md:pt-4 pl-4 md:pl-0 border-border border-t-2 border-l-4 md:border-l-0">
+                            <span className="text-sm font-medium text-rose-600">Step 3</span>
+                            <span className="text-xl font-semibold">Start asking questions</span>
+                            <span className="mt-2 text-secondary-foreground">
+                                It&apos;s that simple. Try out Smarmill today - response time is really great
+                            </span>
+                        </div>
+                    </li>
+                </ol>
+                <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                    <div className="mt-16 sm:mt-24 flow-root">
+                        <div className="bg-gray-900/5 -m-2 lg:-m-4 p-2 lg:p-4 ring-1 ring-inset ring-gray-900/10 rounded-xl">
+                            <Image
+                                className="bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 rounded-md"
+                                src="/images/file-upload-preview.jpg"
+                                alt="uploading preview"
+                                width={1419}
+                                height={732}
+                                quality={100}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
+ 
+export default Home
